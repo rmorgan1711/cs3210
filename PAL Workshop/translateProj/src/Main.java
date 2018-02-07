cdbb8990eface8572fe03bd1b8fb058a7444a258
Jon Bowen <jonathan.bowen@aon.com>
2018-02-06 22:05:00 -0700

    complete label check; add SRT check

--- PAL Workshop/translateProj/src/Main.java
+++ PAL Workshop/translateProj/src/Main.java
@@ -18,56 +18,78 @@ public class Main {
 
         String fileName = args[0];
         List<String> lines = ReadFile(fileName + ".txt");
+        lines = RemoveCommentsAndBlanks(lines);
 
+        Map<ParseError, Integer> errorCount = new HashMap<>();
         List<String> outLines = MakeHeaderLines(fileName);
 
-        int progLine = 1;
+        String[] startLine = lines.get(0).split(" ");
+        if (!startLine[0].equals("SRT")){
+            outLines.add("** error: Program must begin with \"SRT\"");
+            errorCount.put(ParseError.StartError, 1);
+        }else {
+            outLines.add("1.\t" + lines.get(0));
+        }
+
+        int progLine = 2;
+        for (int i = 1; i < lines.size(); i++){
+            String line = lines.get(i);
+
+            outLines.add(progLine + ".\t" + line);
+            progLine++;
+
+            String labelError = CheckLabel(line);
+            if (!labelError.isEmpty()){
+                outLines.add(labelError);
+                errorCount.put(ParseError.LabelError, 1);
+            }
+
+            String[] words = line.split(" ");
+
+        }
+
+        outLines.addAll(MakeFooterLines(errorCount));
+
+        WriteLogFile(fileName + ".log", outLines);
+
+
+    }
+
+    public static List<String> RemoveCommentsAndBlanks(List<String> lines){
+        List<String> contentLines = new ArrayList<>();
+
         for (int i = 0; i < lines.size(); i++){
+
             String line = lines.get(i);
 
             if (line.contains(";"))
                 line = line.substring(0, line.indexOf(';'));
 
             line = line.trim();
-
             if (line.length() == 0)
                 continue;
 
-            String[] words = line.split(" ");
-
-            outLines.add(progLine + ".\t" + line);
-            progLine++;
-
-            if (line.contains(":")){
-                if (!words[0].contains(":")){
-                    System.out.println("labels must leftmost on the line they appear in");
-                    continue;
-                }
-
-                if (words[0].length() - 1 > 5){
-                    System.out.println("labels must be 5 or fewer letters");
-                    continue;
-                }
-
-                if (line.chars().filter(c -> c == ':').count() > 1){
-                    System.out.println("only one label is allowed per line");
-                    continue;
-                }
-
-                if (!words[0].substring(0, line.indexOf(':')).matches("[a-zA-z]+")) {
-                    System.out.println("labels use only a-z and A-Z");
-                    continue;
-                }
-            }
-
-            //System.out.println("bottom of loop");
-
+            contentLines.add(line);
         }
+        return contentLines;
+    }
 
+    public static String CheckLabel(String line){
+        String errorMsg = "";
+        if (line.contains(":")){
+            String label = line.substring(0, line.indexOf(':')).trim();
 
-        WriteLogFile(fileName + ".log", outLines);
-
-
+            if (line.chars().filter(ch -> ch == ':').count() > 1){
+                errorMsg = "** error: Only one label per line";
+            }
+            else if (label.length() > 5){
+                errorMsg = "** error: Lables must be 5 letters or less";
+            }
+            else if (!label.matches("[a-zA-Z]+")){
+                errorMsg = "** error: Lables must use only a-z and A-Z";
+            }
+        }
+        return errorMsg;
     }
 
     public static List<String> ReadFile(String fileName){
@@ -112,7 +134,35 @@ public class Main {
         return header;
     }
 
-    public enum OpType{
+    public static List<String> MakeFooterLines(Map<ParseError, Integer> errorCount){
+        List<String> footer = new ArrayList<>();
+        Map<ParseError, String> errorDescr = MakeErrorDescriptionMap();
+
+        footer.add("\r\nSummary --------------\r\n");
+        int totErrors = errorCount.values().stream().mapToInt(Number::intValue).sum();
+        footer.add("Total errors = " + totErrors + "\r\n");
+
+        for (ParseError key : errorCount.keySet()){
+            String msg = "\t" + errorCount.get(key) + " " + errorDescr.get(key);
+            footer.add(msg);
+        }
+
+        String last = "Processing complete - ";
+        if (totErrors > 0)
+            last += "PAL program is not valid";
+        else
+            last += "PAL program is valid";
+        footer.add(last);
+
+        return footer;
+    }
+
+    public enum ParseError{
+        StartError,
+        LabelError
+    }
+
+    public enum Op {
         Start,
         End,
         OneOp,
@@ -123,22 +173,30 @@ public class Main {
         Branch
     }
 
-    public static Map<String, OpType> OpTypeMap(){
-        Map<String,OpType> opMap = new HashMap<>();
-        opMap.put("SRT", OpType.Start);
-        opMap.put("END", OpType.End);
-        opMap.put("INC", OpType.OneOp);
-        opMap.put("DEC", OpType.OneOp);
-        opMap.put("ADD", OpType.ThreeOp);
-        opMap.put("SUB", OpType.ThreeOp);
-        opMap.put("MUL", OpType.ThreeOp);
-        opMap.put("DIV", OpType.ThreeOp);
-        opMap.put("BEQ", OpType.TestAndBranch);
-        opMap.put("BGT", OpType.TestAndBranch);
-        opMap.put("BR", OpType.Branch);
-        opMap.put("COPY", OpType.Copy);
-        opMap.put("MOVE", OpType.Move);
+    public static Map<String, Op> MakeOpTypeMap(){
+        Map<String, Op> opMap = new HashMap<>();
+        opMap.put("SRT", Op.Start);
+        opMap.put("END", Op.End);
+        opMap.put("INC", Op.OneOp);
+        opMap.put("DEC", Op.OneOp);
+        opMap.put("ADD", Op.ThreeOp);
+        opMap.put("SUB", Op.ThreeOp);
+        opMap.put("MUL", Op.ThreeOp);
+        opMap.put("DIV", Op.ThreeOp);
+        opMap.put("BEQ", Op.TestAndBranch);
+        opMap.put("BGT", Op.TestAndBranch);
+        opMap.put("BR", Op.Branch);
+        opMap.put("COPY", Op.Copy);
+        opMap.put("MOVE", Op.Move);
 
         return  opMap;
+    }
+
+    public static Map<ParseError, String> MakeErrorDescriptionMap(){
+        Map<ParseError, String> eMap = new HashMap<>();
+        eMap.put(ParseError.StartError, "Invalid program start");
+        eMap.put(ParseError.LabelError, "Invalid label");
+
+        return eMap;
     }
 }
