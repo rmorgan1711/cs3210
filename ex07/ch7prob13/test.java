f5370ee02e196289287f43c4393282b79c0b3651
Jon Bowen <jonathan.bowen@aon.com>
2018-03-31 14:54:21 -0600

    ex07

--- ex07/ch7prob13/test.java
+++ ex07/ch7prob13/test.java
@@ -1,7 +1,36 @@
-public class Test{
+public class Test {
 
-	public static void main(String[] args){
-		System.out.println("Hello, World!");
+	public static int funLTR(int[] k){
+		k[0] += 4;
+		
+		return 3 * k[0] - 1;
 	}
 
+	public static int funRTL(int[] k){
+		k[0] += 4;
+
+		return 3 * (k[0] - 1);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Left to right order");
+		int[] i = new int[] { 10 };
+		int[] j = new int[] { 10 };
+		int sum1, sum2;
+		sum1 = (i[0] / 2) + funLTR(i);
+		sum2 = funLTR(j) + (j[0] / 2);	
+
+		System.out.println("sum1 = " + sum1);
+		System.out.println("sum2 = " + sum2);
+
+		System.out.println("Sumulate right to left order");
+		i = new int[] { 10 };
+		j = new int[] { 10 };
+		sum1 = funRTL(i) + (i[0] / 2);
+		sum2 = (j[0] / 2) + funRTL(j);
+
+		System.out.println("sum1 = " + sum1);
+		System.out.println("sum2 = " + sum2);
+
+	}
 }
